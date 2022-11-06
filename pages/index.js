73ec51fbde2140d4af87ecd390e580f2a4e600b0
erdem <erd@tuta.io>
2022-11-06 22:48:34 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script'
 import { withTranslation, i18n } from '../i18n'
 import styles from '../styles/Home.module.css';
 
@@ -6,15 +7,9 @@ function Home({ t }) {
 	return (
 		<div className={styles.container}>
 			<Head>
-			<Script id="google-tag-manager" strategy="afterInteractive">
-      {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-      `}
-    </Script>
+			<Script src="https://www.googletagmanager.com/gtag/js?id=G-9ZVEYFNGEJ" strategy="afterInteractive" />
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`window.dataLayer = window.dataLayer || [];function gtag(){window.dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-9ZVEYFNGEJ');`}</Script>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<title>naaltech - Nevzat Ayaz Anadolu Lisesi Teknoloji Atölyesi</title>
 				<meta name="description" content="naaltech, 2022 yılında Nevzat Ayaz Anadolu Lisesi'nde öğrenciler tarafından okul içerisinde yazılım, robotik ve siber güvenlik alanlarında faaliyetler düzenlemek için kurulmuş bir kulüptür. Kulüp resmi olarak Nevzat Ayaz Anadolu Lisesi altında çalışmaktadır, ayrıca dünya üzerindeki teknoloji kulüplerini destekleyen 'The Hack Foundation'a üyedir." />
