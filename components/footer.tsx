688c320aa5875bee5d98063478945e2224c7f97c
erdem <erdem@ta2edh.com>
2025-08-22 01:53:15 +0300

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -51,7 +51,7 @@ export function Footer() {
           </div>
         </div>
         <div className="border-t border-gray-200 mt-6 pt-6 text-center text-sm text-gray-500">
-          <p className="mb-2">&copy; {new Date().getFullYear()} Nevzat Ayaz Anadolu Lisesi Teknoloji Kulübü. Tüm hakları saklıdır.</p>
+          <p className="mb-2">&copy; {new Date().getFullYear()} Nevzat Ayaz Anadolu Lisesi Teknoloji Kulübü. | Bu proje <a href="https://raw.githubusercontent.com/naaltech/website/refs/heads/main/LICENSE" className="hover:text-gray-600 underline">GPLv3</a> ile lisanslanmıştır.</p>
           <div className="flex flex-col sm:flex-row justify-center items-center gap-2 sm:gap-4 text-xs text-gray-400">
             <p>
               Fiscally sponsored by <a href="https://the.hackfoundation.org/" className="hover:text-gray-600 underline">The Hack Foundation</a>, a 501(c)(3) nonprofit (EIN: 81-2908499)</p>
@@ -65,3 +65,4 @@ export function Footer() {
     </footer>
   )
 }
+
